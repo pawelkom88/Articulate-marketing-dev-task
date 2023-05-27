d9bd9f321a23745a74158d033730d5c21dcfff07
Pawel Komorkiewicz <pawel.komorkiewicz@yahoo.co.uk>
2023-05-27 19:22:58 +0100

    feat: Add logic

---
+++ js/helpers.js
@@ -0,0 +1,62 @@
+export function createCardImage(src, alt) {
+  const cardImage = document.createElement("img");
+  cardImage.className = "product-card__image";
+  cardImage.src = src;
+  cardImage.alt = alt;
+
+  return cardImage;
+}
+
+export function createCardHeading(name) {
+  const cardHeading = document.createElement("h3");
+  cardHeading.className = "product-card__name";
+  cardHeading.textContent = name;
+
+  return cardHeading;
+}
+
+export function createCardPrice(price) {
+  const cardPrice = document.createElement("p");
+  cardPrice.className = "product-card__price";
+  cardPrice.textContent = price;
+
+  return cardPrice;
+}
+
+export function createCardComponent(card) {
+  const productCard = document.createElement("article");
+  productCard.className = "product-card fadein";
+  productCard.setAttribute("tabindex", "0");
+  productCard.setAttribute("aria-label", `product card ${card.alt} - ${card.price}`);
+
+  productCard.appendChild(createCardImage(card.image, card.alt));
+  productCard.appendChild(createCardHeading(card.name));
+  productCard.appendChild(createCardPrice(card.price));
+
+  return productCard;
+}
+
+export function sortByProperty(cards, { property, order }) {
+  const cardsCopy = [...cards];
+  const sortOrder = order === "asc" ? 1 : -1;
+  return cardsCopy.sort((a, b) => {
+    const sortedCards = a[property].localeCompare(b[property]);
+    return sortedCards * sortOrder;
+  });
+}
+
+export function injectCards(cards) {
+  // duplicate ?
+  const productsColumn = document.querySelector(".products-column");
+
+  for (let i = 0; i < cards.length; i++) {
+    const card = createCardComponent(cards[i]);
+    productsColumn.appendChild(card);
+  }
+}
+
+export function updateCards(cards) {
+  const productsColumn = document.querySelector(".products-column");
+  productsColumn.innerHTML = "";
+  injectCards(cards);
+}
--- js/main.js
+++ js/main.js
@@ -1,50 +1,48 @@
-import { cardsData } from "./constants.js";
+import { cardsData } from "./data.js";
+import { sortByProperty, updateCards } from "./helpers.js";
+import { injectCards } from "./helpers.js";
 
-("use strict");
+function init() {
+  injectCards(cardsData);
 
-function createCardImage(src, alt) {
-  let cardImage = document.createElement("img");
-  cardImage.className = "product-card__image";
-  cardImage.src = src;
-  cardImage.alt = alt;
+  const sortByNameBtn = document.querySelector(".sort-by-name-btn");
+  const sortByPriceBtn = document.querySelector(".sort-by-price-btn");
 
-  return cardImage;
+  sortByNameBtn.addEventListener("click", () => {
+    const sortedCardsByNameAscending = sortByProperty(cardsData, {
+      property: "name",
+      order: "asc",
+    });
+
+    updateCards(sortedCardsByNameAscending);
+  });
+
+  sortByNameBtn.addEventListener("dblclick", () => {
+    const sortedCardsByNameDescending = sortByProperty(cardsData, {
+      property: "name",
+      order: "desc",
+    });
+
+    updateCards(sortedCardsByNameDescending);
+  });
+
+  sortByPriceBtn.addEventListener("click", () => {
+    const sortedCardsByPriceAscending = sortByProperty(cardsData, {
+      property: "price",
+      order: "asc",
+    });
+
+    updateCards(sortedCardsByPriceAscending);
+  });
+
+  sortByPriceBtn.addEventListener("dblclick", () => {
+    const sortedCardsByPriceDescending = sortByProperty(cardsData, {
+      property: "price",
+      order: "desc",
+    });
+
+    updateCards(sortedCardsByPriceDescending);
+  });
 }
 
-function createCardHeading(name) {
-  let cardHeading = document.createElement("h3");
-  cardHeading.className = "product-card__name";
-  cardHeading.textContent = name;
-
-  return cardHeading;
-}
-
-function createCardPrice(price) {
-  let cardPrice = document.createElement("p");
-  cardPrice.className = "product-card__price";
-  cardPrice.textContent = price;
-
-  return cardPrice;
-}
-
-function createCardComponent(card) {
-  const productCard = document.createElement("article");
-  productCard.className = "product-card";
-
-  productCard.appendChild(createCardImage(card.image, card.alt));
-  productCard.appendChild(createCardHeading(card.name));
-  productCard.appendChild(createCardPrice(card.price));
-
-  return productCard;
-}
-
-function injectCards(cards) {
-  const productsColumn = document.querySelector(".products-column");
-
-  for (let i = 0; i < cards.length; i++) {
-    let cardElement = createCardComponent(cards[i]);
-    productsColumn.appendChild(cardElement);
-  }
-}
-
-injectCards(cardsData);
+init();
